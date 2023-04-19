cd2ecb866030434a4cadce6b5f4ce281051a3977
Karl Herzog <karl.m.herzog@stud.th-rosenheim.de>
2023-04-19 09:10:51 +0200

    Add dummy class for test purposes

---
+++ PokemonApp/Models/DummyClass.cs
@@ -0,0 +1,18 @@
+﻿namespace PokemonApp.Models
+{
+    public class DummyClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public DummyClass() { }
+
+        public DummyClass(int number, string name)
+        {
+            Number = number;
+            Name = name;
+        }
+    }
+}
+
