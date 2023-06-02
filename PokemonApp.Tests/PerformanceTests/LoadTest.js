c5819b2fe88ee6dc7e7e9010fdf4c75cf58dfb5b
Karl Herzog <karl.m.herzog@stud.th-rosenheim.de>
2023-06-02 22:51:01 +0200

    Add Load Tests

---
+++ PokemonApp.Tests/PerformanceTests/LoadTest.js
@@ -0,0 +1,22 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+    stages: [
+        { duration: '30s', target: 100 }, // continuous ramp-up from 1 to 100 users
+        { duration: '1m', target: 100 }, // stay at 100 users for some time
+        { duration: '30s', target: 0 }, // scale down
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<150'], // 99 % of requests must finish below 150 ms
+    },
+};
+
+export default () => {
+
+    let response = http.get("http://localhost:5099/api/pokemon/1")
+
+    sleep(1);
+}
---
+++ PokemonApp.Tests/PerformanceTests/SpikeTest.js
@@ -0,0 +1,28 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+    stages: [
+        { duration: '10s', target: 100 }, // below normal load
+        { duration: '1m', target: 100 },
+        { duration: '10s', target: 1400 }, // spike of 1400 users
+        { duration: '3m', target: 1400 },  // stay at 1400 users for some time
+        { duration: '10s', target: 100 }, // scale down
+        { duration: '3m', target: 100 },
+        { duration: '10s', target: 0 }, 
+    ]
+};
+
+const API_BASE_URL = 'http://localhost:5099/api/pokemon'
+
+export default () => {
+    http.batch([
+        ['GET', `${API_BASE_URL}/1`],
+        ['GET', `${API_BASE_URL}/543`],
+        ['GET', `${API_BASE_URL}/789`],
+    ]);
+
+    sleep(1);
+}
